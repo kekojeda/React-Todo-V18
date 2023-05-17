56480ef7c4f90cc917007d3c2ad44cbd653a8e5b
Sergio Ojeda <sergio.ojeda@MacBook-Pro-de-Sergio.local>
2023-05-17 20:10:21 -0300

    Add task

--- src/components/TodoForm/TodoForm.js
+++ src/components/TodoForm/TodoForm.js
@@ -1,20 +1,51 @@
+import { useContext, useState } from "react";
 import "./TodoForm.css";
 import { MdAddTask } from "react-icons/md";
-import { MdAdd } from "react-icons/md";
 import { MdClear } from "react-icons/md";
+import { TodoContext } from "../../context/TodoContext";
 
 function TodoForm() {
+  const { item: todos, saveItem: saveTodos } = useContext(TodoContext);
+  const {newTodo, setNewTodo} = useState([])
+
+  let newTodoValue;
+
+  function addTodo(todos, saveTodos){
+    let newTodo = {text: newTodoValue, completed: false}
+    saveTodos([...todos, newTodo])
+
+
+  }
+
   return (
-    <form>
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+      }}
+    >
       <label>Escribe tu nuevo ToDo</label>
-      <textarea placeholder="Ejemplo de nuevo todo" />
+      <textarea 
+      placeholder="Ejemplo de nuevo todo"
+      onChange={(event)=> {
+        newTodoValue=event.target.value
+        console.log(newTodoValue);
+      }
+
+      }
+      // value={(event)=>event.target.value}
+      />
 
       <div className="TodoForm-ButtonCotainer">
         <button className="TodoForm-button TodoForm-button--cancel">
-        <MdClear size={70}/>
+          <MdClear size={70} />
         </button>
-        <button className="TodoForm-button TodoForm-button--add">
-          <MdAddTask size={70}/>
+        <button 
+        className="TodoForm-button TodoForm-button--add" 
+        type="submit"
+        onClick={() => addTodo(todos, saveTodos)}
+        >
+          <MdAddTask size={70} 
+          />
         </button>
       </div>
     </form>
